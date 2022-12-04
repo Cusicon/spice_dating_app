cf7a6e2a3482461553df601ca8b0bf3b49c97265
Success Chukwu <iamcusicon@gmail.com>
2022-12-04 03:27:17 +0100

    fix: changes the Icons

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -60,87 +60,84 @@ class _ProfilePageState extends State<ProfilePage> {
           ),
           FractionalTranslation(
             translation: const Offset(0, -0.5),
-            child: Container(
-              padding: const EdgeInsets.only(left: pagePaddingSize),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  SizedBox(
-                    child: Row(
-                      children: [
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            LineIcons.pause,
-                            color: Theme.of(context).primaryColor,
-                          ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                SizedBox(
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          LineIcons.pause,
+                          color: Theme.of(context).primaryColor,
                         ),
-                        SizedBox(
-                          height: 2.5,
-                          width: 50,
-                          child: LoadingBar(
-                            left: 0.0,
-                            top: 0.0,
-                            right: 0.0,
-                            bottom: 0.0,
-                            backgroundColor: appLightGrey,
-                            color: Theme.of(context).primaryColor,
-                          ),
+                      ),
+                      SizedBox(
+                        height: 2.5,
+                        width: 50,
+                        child: LoadingBar(
+                          left: 0.0,
+                          top: 0.0,
+                          right: 0.0,
+                          bottom: 0.0,
+                          backgroundColor: appLightGrey,
+                          color: Theme.of(context).primaryColor,
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: pagePaddingSize,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        CustomIconButton(
-                          color: appNormalGrey,
-                          height: 75.0,
-                          width: 75.0,
-                          relativeIconSizeToParentHeight: 0.5,
-                          border: Border.all(
-                            width: borderWidth * 4,
-                            color: Theme.of(context).scaffoldBackgroundColor,
-                          ),
-                          borderedRadius: const BorderRadius.all(
-                            Radius.circular(100),
-                          ),
-                          onPressed: () {},
-                          child: const Icon(
-                            LineIcons.columns,
-                            color: Colors.white,
-                          ),
-                        ),
-                        const SizedBox(width: minSizedBox),
-                        CustomIconButton(
-                          color: const Color(appPrimaryColor),
-                          height: 100.0,
-                          width: 100.0,
-                          relativeIconSizeToParentHeight: 0.5,
-                          border: Border.all(
-                            width: borderWidth * 4,
-                            color: Theme.of(context).scaffoldBackgroundColor,
-                          ),
-                          borderedRadius: const BorderRadius.all(
-                            Radius.circular(100),
-                          ),
-                          onPressed: () {},
-                          child: const Icon(
-                            LineIcons.commentDots,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: pagePaddingSize,
                   ),
-                ],
-              ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      CustomIconButton(
+                        color: appNormalGrey,
+                        height: 75.0,
+                        width: 75.0,
+                        relativeIconSizeToParentHeight: 0.5,
+                        border: Border.all(
+                          width: borderWidth * 4,
+                          color: Theme.of(context).scaffoldBackgroundColor,
+                        ),
+                        borderedRadius: const BorderRadius.all(
+                          Radius.circular(100),
+                        ),
+                        onPressed: () {},
+                        child: const Icon(
+                          LineIcons.photoVideo,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(width: minSizedBox),
+                      CustomIconButton(
+                        color: const Color(appPrimaryColor),
+                        height: 100.0,
+                        width: 100.0,
+                        relativeIconSizeToParentHeight: 0.5,
+                        border: Border.all(
+                          width: borderWidth * 4,
+                          color: Theme.of(context).scaffoldBackgroundColor,
+                        ),
+                        borderedRadius: const BorderRadius.all(
+                          Radius.circular(100),
+                        ),
+                        onPressed: () {},
+                        child: const Icon(
+                          LineIcons.commentDots,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           )
         ],
