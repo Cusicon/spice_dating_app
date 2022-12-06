445b4b56dabb80cba2bdc58fb69adf05ece184ab
Success Chukwu <iamcusicon@gmail.com>
2022-12-06 11:05:19 +0100

    used shimmer and refactored

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
 import 'package:palette_generator/palette_generator.dart';
 
+import '../extensions/string_api_ext.dart';
 import '../models/user_model.dart';
+import '../utils/colors.dart';
 import '../utils/constants.dart';
 import '../widgets/widgets.dart';
 
@@ -46,64 +49,178 @@ class _ProfilePageState extends State<ProfilePage> {
     _getPaletteColors(currentUser.photos.first);
 
     return Scaffold(
-      body: ListView(
-        shrinkWrap: true,
-        controller: widget.scrollController,
-        children: [
-          ProfileHeadPhotos(
-            height: headHeight,
-            user: currentUser,
-            paletteColor: paletteColor.color,
-          ),
-          const ProfileMainActions(),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: minPaddingSize * 2),
-            height: MediaQuery.of(context).size.height,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Text(
-                            '${currentUser.name}, ${currentUser.age}',
-                            style: Theme.of(context).textTheme.headlineSmall,
+      body: Container(
+        color: Colors.transparent,
+        child: ListView(
+          shrinkWrap: true,
+          controller: widget.scrollController,
+          children: [
+            ProfileHeadPhotos(
+              height: headHeight,
+              user: currentUser,
+              paletteColor: paletteColor.color,
+            ),
+            ProfileMainActions(
+              bgPaletteColor: Theme.of(context).scaffoldBackgroundColor,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: pagePaddingSize),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ProfileBioDetails(currentUser: currentUser),
+                  const SizedBox(height: maxSizedBox * 2),
+                  Opacity(
+                    opacity: 0.8,
+                    child: Container(
+                      padding: const EdgeInsets.all(pagePaddingSize),
+                      decoration: BoxDecoration(
+                        border: Border.symmetric(
+                          horizontal: BorderSide(
+                            color: appLightGrey,
                           ),
-                          const SizedBox(width: minSizedBox),
-                          const Image(
-                            height: maxSizedBox,
-                            image: AssetImage(
-                              'assets/icons/verified-badge.png',
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Icon(
+                                  LineIcons.rulerVertical,
+                                  size: 36.0,
+                                  color: appPrimaryColor,
+                                ),
+                                const SizedBox(width: minSizedBox),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Height',
+                                      style:
+                                          Theme.of(context).textTheme.caption,
+                                    ),
+                                    const SizedBox(height: minSizedBox),
+                                    Text(
+                                      '${currentUser.height} Cm',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .titleSmall,
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Icon(
+                                  LineIcons.ring,
+                                  size: 36.0,
+                                  color: appPrimaryColor,
+                                ),
+                                const SizedBox(width: minSizedBox),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Relationship Status',
+                                      style:
+                                          Theme.of(context).textTheme.caption,
+                                    ),
+                                    const SizedBox(height: minSizedBox),
+                                    Text(
+                                      currentUser.maritalStatus.name
+                                          .toNameCase(),
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .titleSmall,
+                                    ),
+                                  ],
+                                )
+                              ],
                             ),
                           ),
                         ],
                       ),
-                      Text(currentUser.city),
-                    ],
+                    ),
                   ),
-                ),
-                const SizedBox(height: maxSizedBox * 3),
-                SizedBox(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Bio',
-                        style: Theme.of(context).textTheme.caption,
-                      ),
-                      const SizedBox(height: maxSizedBox),
-                      Text(
-                        currentUser.bio * 10,
-                        style: Theme.of(context).textTheme.bodyMedium,
-                      )
-                    ],
+                  const SizedBox(height: maxSizedBox * 2),
+                  GridView.builder(
+                    primary: false,
+                    shrinkWrap: true,
+                    itemCount: currentUser.photos.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      mainAxisExtent: MediaQuery.of(context).size.height * 0.25,
+                    ),
+                    itemBuilder: (ctx, index) {
+                      List<String> userPhotos = currentUser.photos;
+
+                      return InkWell(
+                        // onTap: () => selectUser(index),
+                        onTap: () {},
+                        child: Padding(
+                          padding: const EdgeInsets.all(minPaddingSize),
+                          child: ImageContainer(imageUrl: userPhotos[index]),
+                        ),
+                      );
+                    },
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileBioDetails extends StatelessWidget {
+  const ProfileBioDetails({
+    Key? key,
+    required this.currentUser,
+  }) : super(key: key);
+
+  final User currentUser;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(
+                '${currentUser.name}, ${currentUser.age}',
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+              const SizedBox(width: minSizedBox),
+              const Image(
+                height: maxSizedBox,
+                image: AssetImage(
+                  'assets/icons/verified-badge.png',
+                ),
+              ),
+            ],
+          ),
+          Text(currentUser.city),
+          const SizedBox(height: pagePaddingSize * 2),
+          Text(
+            'Bio',
+            style: Theme.of(context).textTheme.caption,
+          ),
+          const SizedBox(height: minSizedBox),
+          Text(
+            currentUser.bio,
+            // '${currentUser.bio}. ' * 50,
+            style: Theme.of(context).textTheme.bodyMedium,
           ),
         ],
       ),
